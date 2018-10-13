426d340bc7d61e97c19efd26a41f60e247906cdc
cw <1801210816@pku.edu.cn>
2018-10-13 09:55:31 +0800

    network ask

--- app/src/main/java/com/example/dell/pkuweather/MainActivity.java
+++ app/src/main/java/com/example/dell/pkuweather/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.dell.pkuweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.dell.util.NetUtil;
 
 public class MainActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        if (NetUtil.getNetworkState(this)!=NetUtil.NETWORK_NONE){
+            Log.d("myWeather","网络OK");
+            Toast.makeText(MainActivity.this,"网络OK",Toast.LENGTH_LONG).show();
+
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/dell/util/NetUtil.java
@@ -0,0 +1,30 @@
+package com.example.dell.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+public class NetUtil {
+    public static final int NETWORK_NONE = 0;
+    public static final int NETWORK_WIFI = 0;
+    public static final int NETWORK_MOBILE = 0;
+    public static int getNetworkState(Context context){
+        ConnectivityManager conManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = conManager.getActiveNetworkInfo();//出错 解决方案：在AndroidManifest
+        //文件中添加用户访问网络权限 <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+//https://blog.csdn.net/hack8/article/details/28038541
+        if (networkInfo == null){
+            return NETWORK_NONE;
+        }
+
+        int nType = networkInfo.getType();
+        if(nType == ConnectivityManager.TYPE_MOBILE){
+            return NETWORK_WIFI;
+        }else if (nType==ConnectivityManager.TYPE_WIFI){
+            return NETWORK_WIFI;
+        }
+        return NETWORK_NONE;
+    }
+}
